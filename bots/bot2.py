e2ec4b03eb01dd0bdac518ca766caaef43d594c0
ponitka <43109956+ponitka@users.noreply.github.com>
2018-09-21 23:50:31 +0200

    Updated bots

--- bots/bot2.py
+++ bots/bot2.py
@@ -17,45 +17,88 @@ class Bot2(Player):
     self.bot = True
 
   def evaluate(self, array):
+    new_array = [[0 for i in range(self.size_of_board)] for j in range(self.size_of_board)]
+  
     result = 0
     for i in range(self.size_of_board):
       for j in range(self.size_of_board):
         for d in [[0, 1], [1, 0], [1, 1], [-1, 1]]:
           
           tab = [0, 0, 0]
-          full = 1
+          full = True
           for r in range(5):
             if i+d[0]*r < 0 or i+d[0]*r >= self.size_of_board:
-              full = 0
+              full = False
               break
             if j+d[1]*r < 0 or j+d[1]*r >= self.size_of_board:
-              full = 0
+              full = False
               break
-            tab[array[i+d[0]*r][j+d[1]*r]] += 1
+            tab[self.array[i+d[0]*r][j+d[1]*r]] += 1
 
-          if full == 0:
+          if full == False or (tab[0] > 0 and tab[1] > 0) or (tab[0] == 0 and tab[1] == 0):
             continue
 
-          if tab[0] == 0 and tab[1] == 1:
+          if tab[1] == 1:
             result += 1
-          if tab[0] == 0 and tab[1] == 2:
+          if tab[1] == 2:
             result += 4
-          if tab[0] == 0 and tab[1] == 3:
+          if tab[1] == 3:
             result += 500
-          if tab[0] == 0 and tab[1] == 4:
-            result += 1000000
+          if tab[1] == 4:
+            result += (int)(1e16)
 
-          if tab[1] == 0 and tab[0] == 1:
+          if tab[0] == 1:
             result -= 0.05
-          if tab[1] == 8 and tab[0] == 2:
+          if tab[0] == 2:
             result -= 2
-          if tab[1] == 0 and tab[0] == 3:
+          if tab[0] == 3:
             result -= 10
-          if tab[1] == 0 and tab[0] == 4:
+          if tab[0] == 4:
             result -= 20
-          if tab[1] == 0 and tab[0] == 5:
-            result -= 10000000000
+          if tab[0] == 5:
+            result -= (int)(1e18)
 
+          if tab[0] == 4:
+            ok = True
+            if i-d[0] < 0 or i-d[0] >= self.size_of_board:
+              ok = False
+            if j-d[1] < 0 or j-d[1] >= self.size_of_board:
+              ok = False
+            if ok == True:
+              if self.array[i-d[0]][j-d[1]] == 2 and self.array[i+4*d[0]][j+4*d[1]] == 2:
+                result -= (int)(1e14)
+
+          if tab[1] == 3:
+            ok = True
+            if i-d[0] < 0 or i-d[0] >= self.size_of_board:
+              ok = False
+            if j-d[1] < 0 or j-d[1] >= self.size_of_board:
+              ok = False
+            if ok == True:
+              if self.array[i-d[0]][j-d[1]] == 2 and self.array[i+4*d[0]][j+4*d[1]] == 2:
+                result += (int)(1e12)
+                
+          if tab[1] == 3:
+            for r in range(5):
+              if self.array[i+r*d[0]][j+r*d[1]] == 2:
+                new_array[i+r*d[0]][j+r*d[1]] += 1
+                
+          if tab[1] == 2:
+            ok = True
+            if i-d[0] < 0 or i-d[0] >= self.size_of_board:
+              ok = False
+            if j-d[1] < 0 or j-d[1] >= self.size_of_board:
+              ok = False
+            if ok == True:
+              if self.array[i-d[0]][j-d[1]] == 2 and self.array[i+4*d[0]][j+4*d[1]] == 2:
+                for r in range(4):
+                  new_array[i+r*d[0]][j+r*d[1]] += 1
+                
+    for i in range(self.size_of_board):
+      for j in range(self.size_of_board):
+        if array[i][j] == 2 and new_array[i][j] >= 2:
+          result += (int)(1e12)          
+              
     return result
 
   def make_move(self):
@@ -67,7 +110,7 @@ class Bot2(Player):
       for j in range(self.size_of_board):
         self.array[i].append(self.GameBoard.GetSquare(i,j))
 
-    best_eva = 10000000000
+    best_eva = (int)(1e20)
     solutions = []
 
     for i in range(self.size_of_board):
@@ -86,3 +129,4 @@ class Bot2(Player):
     index = random.randint(0, len(solutions)-1)
 
     self.GameBoard.MakeMove(self, solutions[index][0], solutions[index][1])
+
